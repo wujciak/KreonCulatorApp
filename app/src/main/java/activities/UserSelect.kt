b8a7f04e5d6808ec65678d9a330dfaef7aaf9b5c
Jakub <jakub.wojciak2002@gmail.com>
2024-03-30 10:16:57 +0100

    Added activity change button.

--- app/src/main/java/activities/UserSelect.kt
+++ app/src/main/java/activities/UserSelect.kt
@@ -1,7 +1,9 @@
 package activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -27,5 +29,12 @@ class UserSelect : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val buttonClick = findViewById<Button>(R.id.newUserButton)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, CreateUser::class.java)
+            startActivity(intent)
+        }
+
     }
 }
