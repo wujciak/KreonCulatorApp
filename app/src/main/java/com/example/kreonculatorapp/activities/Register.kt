2c68f3bb3006070696d601cb43ba43ce8c0c874e
Jakub <jakub.wojciak2002@gmail.com>
2024-04-09 00:34:11 +0200

    Improved activity change from register to login and from login to Main. Added hint to textViews.

--- app/src/main/java/com/example/kreonculatorapp/activities/Register.kt
+++ app/src/main/java/com/example/kreonculatorapp/activities/Register.kt
@@ -1,21 +1,60 @@
 package com.example.kreonculatorapp.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.kreonculatorapp.R
 
 class Register : AppCompatActivity() {
+    private lateinit var inputName: EditText
+    private lateinit var inputEmail: EditText
+    private lateinit var inputPassword: EditText
+    private lateinit var inputPasswordRepeat: EditText
+    private lateinit var registerButton: Button
+    private lateinit var logInText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        enableEdgeToEdge()
+        initializeViews()
+        setupListeners()
+    }
+
+    private fun initializeViews() {
+        supportActionBar?.title = "Register"
+        inputName = findViewById(R.id.inputName)
+        inputEmail = findViewById(R.id.inputEmail)
+        inputPassword = findViewById(R.id.inputPassword)
+        inputPasswordRepeat = findViewById(R.id.inputPasswordRepeat)
+        registerButton = findViewById(R.id.registerButton)
+        logInText = findViewById(R.id.logInText)
+    }
+
+    private fun setupListeners() {
+
+        registerButton.setOnClickListener {
+            val name = inputName.text.toString()
+            val email = inputEmail.text.toString()
+            val password = inputPassword.text.toString()
+            val passwordRepeat = inputPasswordRepeat.text.toString()
+            if (name.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && passwordRepeat == password) {
+                val intent = Intent(this, SignIn::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Please fill all fields!", Toast.LENGTH_SHORT).show()
+            }
         }
+
+        logInText.setOnClickListener {
+            val intent = Intent(this, SignIn::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/kreonculatorapp/activities/SignIn.kt
+++ app/src/main/java/com/example/kreonculatorapp/activities/SignIn.kt
@@ -1,6 +1,12 @@
 package com.example.kreonculatorapp.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,14 +14,36 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.kreonculatorapp.R
 
 class SignIn : AppCompatActivity() {
+    private lateinit var inputEmailSignIn: EditText
+    private lateinit var inputPasswordSignIn: EditText
+    private lateinit var loginButtonSignIn: Button
+    private lateinit var registerTextViewClickableSignIn: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_sign_in)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        enableEdgeToEdge()
+        initializeViews()
+        setupListeners()
+    }
+
+    private fun initializeViews() {
+        supportActionBar?.title = "Sign In"
+        inputEmailSignIn = findViewById(R.id.inputEmailSignIn)
+        inputPasswordSignIn = findViewById(R.id.inputPasswordSignIn)
+        loginButtonSignIn = findViewById(R.id.loginButtonSignIn)
+        registerTextViewClickableSignIn = findViewById(R.id.registerTextViewClickableSignIn)
+    }
+
+    private fun setupListeners() {
+        loginButtonSignIn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        registerTextViewClickableSignIn.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
         }
     }
 }
