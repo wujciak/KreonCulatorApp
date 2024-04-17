ea7c49e97f94994e76e41d5864f6ecd80a4c0b0e
Jakub <jakub.wojciak2002@gmail.com>
2024-04-17 23:04:53 +0200

    Simple register functionality

--- app/src/main/java/com/example/kreonculatorapp/activities/Register.kt
+++ app/src/main/java/com/example/kreonculatorapp/activities/Register.kt
@@ -2,6 +2,8 @@ package com.example.kreonculatorapp.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -9,6 +11,8 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kreonculatorapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class Register : AppCompatActivity() {
     private lateinit var inputName: EditText
@@ -23,7 +27,9 @@ class Register : AppCompatActivity() {
         setContentView(R.layout.activity_register)
         enableEdgeToEdge()
         initializeViews()
-        setupListeners()
+        registerButton.setOnClickListener{
+            registerUser()
+        }
     }
 
     private fun initializeViews() {
@@ -36,24 +42,53 @@ class Register : AppCompatActivity() {
         logInText = findViewById(R.id.logInText)
     }
 
-    private fun setupListeners() {
-        registerButton.setOnClickListener {
-            val name = inputName.text.toString()
-            val email = inputEmail.text.toString()
-            val password = inputPassword.text.toString()
-            val passwordRepeat = inputPasswordRepeat.text.toString()
-            if (name.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && passwordRepeat == password) {
-                val intent = Intent(this, SignIn::class.java)
-                startActivity(intent)
-            } else {
-                Toast.makeText(this, "Please fill all fields!", Toast.LENGTH_SHORT).show()
+    private fun validateRegisterDetails(): Boolean {
+        return when {
+            TextUtils.isEmpty(inputEmail.text.toString().trim{ it <= ' '}) -> {
+                Toast.makeText(this, "Please fill email field!", Toast.LENGTH_SHORT).show()
+                false
             }
+            TextUtils.isEmpty(inputName.text.toString().trim{ it <= ' '}) -> {
+                Toast.makeText(this, "Please fill name field!", Toast.LENGTH_SHORT).show()
+                false
+            }
+            TextUtils.isEmpty(inputPassword.text.toString().trim{ it <= ' '}) -> {
+                Toast.makeText(this, "Please fill password field!", Toast.LENGTH_SHORT).show()
+                false
+            }
+            TextUtils.isEmpty(inputPasswordRepeat.text.toString().trim{ it <= ' '}) -> {
+                Toast.makeText(this, "Please fill repeat password field!", Toast.LENGTH_SHORT).show()
+                false
+            }
+            inputPassword.text.toString().trim {it <= ' '} != inputPasswordRepeat.text.toString().trim{it <= ' '} -> {
+                Toast.makeText(this, "Passwords don't match!", Toast.LENGTH_SHORT).show()
+                false
+            }
+            else -> true
         }
+    }
 
-        logInText.setOnClickListener {
-            val intent = Intent(this, SignIn::class.java)
-            startActivity(intent)
+    fun goToLogin(view: View) {
+        val intent = Intent(this, SignIn::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun registerUser() {
+        if (validateRegisterDetails()) {
+            val login: String = inputEmail.text.toString().trim {it <= ' '}
+            val password: String = inputPassword.text.toString().trim {it <= ' '}
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(login, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val firebaseUser: FirebaseUser = task.result!!.user!!
+                        Toast.makeText(this, "You are registered successfully." +
+                                    " Your user id is ${firebaseUser.uid}", Toast.LENGTH_SHORT).show()
+                        FirebaseAuth.getInstance().signOut()
+                        finish()
+                    }
+                }
         }
-
     }
 }
