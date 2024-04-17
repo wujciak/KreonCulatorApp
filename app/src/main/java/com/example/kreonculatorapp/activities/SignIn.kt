46a1cbb317a00e591f70780ddc6ff50513841fd9
Jakub <jakub.wojciak2002@gmail.com>
2024-04-17 13:39:17 +0200

    Hardcore strings correction

--- app/src/main/java/com/example/kreonculatorapp/activities/SignIn.kt
+++ app/src/main/java/com/example/kreonculatorapp/activities/SignIn.kt
@@ -2,15 +2,11 @@ package com.example.kreonculatorapp.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.kreonculatorapp.R
 
 class SignIn : AppCompatActivity() {
