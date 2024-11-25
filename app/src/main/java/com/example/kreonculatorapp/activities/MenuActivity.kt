9700edb1009c99122837d55066f4ba5e40b65a37
Jakub <jakub.wojciak2002@gmail.com>
2024-11-25 15:42:08 +0100

    initial commit

--- app/src/main/java/com/example/kreonculatorapp/activities/MenuActivity.kt
+++ app/src/main/java/com/example/kreonculatorapp/activities/MenuActivity.kt
@@ -16,6 +16,7 @@ class MenuActivity : AppCompatActivity() {
         // Inicjalizacja przycisków
         val obtainButton = findViewById<Button>(R.id.obtainButton)
         val mapButton = findViewById<Button>(R.id.mapButton)
+        val chatButton = findViewById<Button>(R.id.chatButton)
 
         // Ustawienie paddingu dla głównego widoku w oparciu o systemowe marginesy
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -33,6 +34,12 @@ class MenuActivity : AppCompatActivity() {
         // Ustawienie OnClickListener dla przycisku mapButton - przejście do MapsActivity
         mapButton.setOnClickListener {
             val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Ustawienie OnClickListener dla przycisku chatButton - przejście do ChatActivity
+        chatButton.setOnClickListener {
+            val intent = Intent(this, ChatActivity::class.java)
             startActivity(intent)
         }
     }
